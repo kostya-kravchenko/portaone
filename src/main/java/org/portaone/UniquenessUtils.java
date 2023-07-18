167d843ccc716e269256f62b380e68550c7af1ae
kostyakravchenko <dev.java.online@gmail.com>
2023-07-18 14:11:27 +0300

    update

--- src/main/java/org/portaone/UniquenessUtils.java
+++ src/main/java/org/portaone/UniquenessUtils.java
@@ -2,24 +2,13 @@ package org.portaone;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.awt.*;
 import java.util.Arrays;
-import java.util.regex.MatchResult;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
 public class UniquenessUtils {
 
     public static String getUniqueChar(String text) {
-//        String[] split = text.split("[\\s\\p{Punct}]+");
-//        return getFirstUnique(Pattern.compile("[\\w'.+]+")
-//                .matcher(text)
-//                .results()
-//                .map(MatchResult::group)
-//                .map(UniquenessUtils::getFirstUnique)
-//                .collect(Collectors.joining()));
-
         return getFirstUnique(Arrays.stream(text.split("[\\s\\p{Punct}]+"))
                 .map(UniquenessUtils::getFirstUnique)
                 .collect(Collectors.joining()));
--- src/test/java/UniqueCharTest.java
+++ src/test/java/UniqueCharTest.java
@@ -13,24 +13,23 @@ public class UniqueCharTest {
 
     public static Object[][] dataProvider() {
         return new Object[][]{
-//                {"e", "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup R# R++"},
-//                {"m", "The Tao gave birth to machine language.  Machine language gave birth\n" +
-//                        "to the assembler.\n" +
-//                        "The assembler gave birth to the compiler.  Now there are ten thousand\n" +
-//                        "languages.\n" +
-//                        "Each language has its purpose, however humble.  Each language\n" +
-//                        "expresses the Yin and Yang of software.  Each language has its place within\n" +
-//                        "the Tao.\n" +
-//                        "But do not program in COBOL if you can avoid it.\n" +
-//                        "        -- Geoffrey James, \"The Tao of Programming\""},
-//                {"", ""},
-//                {"1", "1"},
-//                {"W", "QQQ EWE TTT"},
-//                {"", "QWQ EWE TWT"},
+                {"e", "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup R# R++"},
+                {"m", "The Tao gave birth to machine language.  Machine language gave birth\n" +
+                        "to the assembler.\n" +
+                        "The assembler gave birth to the compiler.  Now there are ten thousand\n" +
+                        "languages.\n" +
+                        "Each language has its purpose, however humble.  Each language\n" +
+                        "expresses the Yin and Yang of software.  Each language has its place within\n" +
+                        "the Tao.\n" +
+                        "But do not program in COBOL if you can avoid it.\n" +
+                        "        -- Geoffrey James, \"The Tao of Programming\""},
+                {"", ""},
+                {"1", "1"},
+                {"W", "QQQ EWE TTT"},
+                {"", "QWQ EWE TWT"},
                 {"H", "Hello World"},
                 {"r", "bbookkeepper"},
-//                {"T", "Testing"},
-//                {"O", "OpenAI GPT-3.5!"},
+                {"T", "Testing"},
         };
     }
 }
